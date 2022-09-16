aa0572ea11a976b20ae785683be5a152963ef10b
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-17 00:07:16 +0530

    Fix news content

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,31 +1,28 @@
 import React from "react";
 import classes from "./styles/NewsItem.module.css";
 import imageplaceholder from "../assets/images/imageplaceholder.png";
+
 const NewsItem = ({ news }) => {
-  return (
-    <div className={classes.NewsItem}>
-      <div className={classes.NewsItem__Image}>
-        <img
-          src={news.urlToImage ? news.urlToImage : imageplaceholder}
-          alt=""
-        />
-      </div>
-      <div className={classes.NewsItem__Description}>
-        <div className={classes.NewsItem__Title}>{news.title}</div>
-        <div className={classes.NewsItem__PublishedOn}>
-          <strong>short</strong> by{" "}
-          {news.author ? news.author : "Anonymous User"} / {news.publishedAt}
+    return (
+        <div className={classes.NewsItem}>
+            <div className={classes.NewsItem__Image}>
+                <img src={news.urlToImage ? news.urlToImage : imageplaceholder} alt="" />
+            </div>
+            <div className={classes.NewsItem__Description}>
+                <div className={classes.NewsItem__Title}>{news.title}</div>
+                <div className={classes.NewsItem__PublishedOn}>
+                    <strong>short</strong> by {news.author ? news.author : "Anonymous User"} / {news.publishedAt}
+                </div>
+                <div className={classes.NewsItem__Content} dangerouslySetInnerHTML={{ __html: news.content }}></div>
+                <div className={classes.NewsItem__Source}>
+                    read more at{" "}
+                    <a href={news.url} rel="noreferrer" target="_blank">
+                        {news.source.name ? news.source.name : "here"}
+                    </a>
+                </div>
+            </div>
         </div>
-        <div className={classes.NewsItem__Content}>{news.content}</div>
-        <div className={classes.NewsItem__Source}>
-          read more at{" "}
-          <a href={news.url} rel="noreferrer" target="_blank">
-            {news.source.name ? news.source.name : "here"}
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default NewsItem;
