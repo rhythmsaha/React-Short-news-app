76616abaf97937a8087ed73da0856d04f6da7563
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-17 00:19:34 +0530

    migrated to vite

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./styles/Footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={classes.Footer}>
+            <div>
+                Clone built by <a href="https://rhythmsaha.me">Rhythm Saha</a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
